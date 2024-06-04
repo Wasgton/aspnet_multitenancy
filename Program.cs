410d3803923413ac93e8dad961c0151a458b94ed
Wasgton Rodrigues Junior <wasgton.it@gmail.com>
2024-06-04 07:35:57 -0300

    fix: Removing https redirection

--- Program.cs
+++ Program.cs
@@ -20,7 +20,6 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseMiddleware<TenantResolver>();
 app.MapControllers();
