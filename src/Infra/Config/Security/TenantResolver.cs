50605056591e026cf1cce44d3c4e2c07261995ec
Wasgton Rodrigues Junior <wasgton.it@gmail.com>
2024-06-04 07:36:46 -0300

    fix: TenantResolver
    Adding a validation to the middleware handle when the tenant id is not informed

--- src/Infra/Config/Security/TenantResolver.cs
+++ src/Infra/Config/Security/TenantResolver.cs
@@ -1,4 +1,5 @@
 using multitenancy.Application.Repositories;
+using Newtonsoft.Json;
 
 namespace multitenancy.Infra.Config.Security;
 
@@ -14,11 +15,18 @@ public class TenantResolver
     public async Task InvokeAsync(HttpContext context, ICurrentTenant currentTenant)
     {
         context.Request.Headers.TryGetValue("tenant", out var tenantFromHeader);
-        if (string.IsNullOrEmpty(tenantFromHeader) == false)
+        if (string.IsNullOrEmpty(tenantFromHeader))
+        {
+            context.Response.Clear();
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(new {Error = context.Response.StatusCode, Message = "Tenant not informed"}));
+        }
+        else
         {
             var parsedTenantId = Guid.Parse(tenantFromHeader!);
             await currentTenant.SetTenant(parsedTenantId);
+            await _next(context);
         }
-        await _next(context);
     }
 }
